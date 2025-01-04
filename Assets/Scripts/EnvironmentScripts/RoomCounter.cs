0016bf08815a9561b011c341b1e20a7bc4264cda
Mohammed Yusuf Ahmed <yusufahmed3435@outlook.com>
2025-01-04 18:15:02 +0000

    Added PaperClip currency to the game
    -Added Large, Medium and small paper clip items
    -Update the UI Text when the currency is changed.

--- Assets/Scripts/EnvironmentScripts/RoomCounter.cs
+++ Assets/Scripts/EnvironmentScripts/RoomCounter.cs
@@ -2,29 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro; // Import TextMeshPro namespace
 
 public class RoomCounter : MonoBehaviour
 {
     public int roomCount = 1;
-    private TextMeshProUGUI roomCountText; // Private reference to the UI Text element
     void Start()
     {
-        // Find the Text component in the children of this GameObject
-        roomCountText = GetComponentInChildren<TextMeshProUGUI>();
-
-        if (roomCountText == null)
-        {
-            Debug.LogError("RoomCountText (Text UI) is not found as a child of RoomCounter.");
-        }
     }
 
     void Update()
     {
-         if (roomCountText != null)
-        {
-            roomCountText.text = "Room: " + roomCount; // Set the text to show the current room count
-        }
         
     }
 }
