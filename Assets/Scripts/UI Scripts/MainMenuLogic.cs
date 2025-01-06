7d6abe35a9c47db50ea6302bd4d45a39cffa21fb
Fahim <139593307+maybe-fahim@users.noreply.github.com>
2025-01-06 01:14:43 +0000

    Library scene updated
    Added library puzzle instructions

--- Assets/Scripts/UI Scripts/MainMenuLogic.cs
+++ Assets/Scripts/UI Scripts/MainMenuLogic.cs
@@ -27,7 +27,7 @@ public class MainMenuLogic : MonoBehaviour
         loading.GetComponent<Canvas>().enabled = true;
         mainMenu.GetComponent<Canvas>().enabled = false;
         buttonSound.Play();
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("Room_PCG");
     }
 
     public void SettingsButton()
